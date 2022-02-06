bb39c8ab57fbf357bfe2e1c71104470940daa156
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-02-06 09:43:45 +0300

    Time: 1372 ms (6.36%), Space: 119.1 MB (12.29%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,6 +1,8 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        HashSet<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> triplets = new ArrayList<>();
+        HashSet<String> set = new HashSet<>();
+        
         HashMap<Integer, Integer> indices = new HashMap<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++){
@@ -11,12 +13,17 @@ class Solution {
             for(int j = i + 1; j < nums.length; j++){
                 int target = 0 - (nums[i] + nums[j]);
                 int targetIdx = indices.getOrDefault(target, -1);
+                
                 if(targetIdx > j){
-                    set.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[targetIdx])));
+                    String signature = nums[i] + "," + nums[j] + "," + nums[targetIdx];
+                    if(!set.contains(signature)){
+                        triplets.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[targetIdx])));
+                        set.add(signature);
+                    }
                 }
             }
         }
         
-        return new ArrayList<>(set);
+        return triplets;
     }
 }
