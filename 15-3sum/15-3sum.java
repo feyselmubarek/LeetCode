a259fbf8fda2157e2a2f46acd66152d33681a432
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-02-06 09:52:36 +0300

    Time: 1056 ms (9.30%), Space: 201.4 MB (5.01%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>();
-        HashSet<String> set = new HashSet<>();
-        
-        HashMap<Integer, Integer> indices = new HashMap<>();
+        HashSet<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            indices.put(nums[i], i);
-        }
         
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                int target = 0 - (nums[i] + nums[j]);
-                int targetIdx = indices.getOrDefault(target, -1);
-                
-                if(targetIdx > j){
-                    String signature = nums[i] + "," + nums[j] + "," + nums[targetIdx];
-                    if(!set.contains(signature)){
-                        triplets.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[targetIdx])));
-                        set.add(signature);
+            int sum = 0 - nums[i];
+            int lo = i + 1, hi = nums.length - 1;
+            while(lo < hi){
+                int maybe = nums[lo] + nums[hi];
+                if(maybe == sum){
+                    List<Integer> triple = new ArrayList<>(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    if(!set.contains(triple)){
+                        set.add(triple);
+                        triplets.add(triple);
                     }
+                }
+                if(maybe < sum){
+                    lo++;
+                }else{
+                    hi--;
                 }
             }
         }
