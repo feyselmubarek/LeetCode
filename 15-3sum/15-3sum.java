4786621b65d8da1bd966af6499154503e0eafc2e
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-02-06 09:59:16 +0300

    Time: 203 ms (30.03%), Space: 120.9 MB (11.48%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,25 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
+        HashSet<Integer> first = new HashSet<>();
         Arrays.sort(nums);
         
         for(int i = 0; i < nums.length; i++){
+            if(first.contains(nums[i])) 
+                continue;
+            
+            first.add(nums[i]);
             int sum = 0 - nums[i];
             int lo = i + 1, hi = nums.length - 1;
+            
+            HashSet<Integer> second = new HashSet<>();
+            
             while(lo < hi){
                 int maybe = nums[lo] + nums[hi];
-                if(maybe == sum){
-                    List<Integer> triple = new ArrayList<>(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                    if(!set.contains(triple)){
-                        set.add(triple);
-                        triplets.add(triple);
-                    }
-                }
-                if(maybe < sum){
+                
+                if(second.contains(nums[lo]) || maybe < sum){
                     lo++;
-                }else{
+                }else if(second.contains(nums[hi]) || maybe > sum){
                     hi--;
+                }else {
+                    List<Integer> triple = new ArrayList<>(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    second.addAll(Arrays.asList(nums[lo], nums[hi]));
+                    triplets.add(triple);
                 }
             }
         }
