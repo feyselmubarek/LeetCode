4c3b8c5f85fb82921b38d236433b002806abcba6
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-02-22 16:35:43 +0300

    Time: 114 ms (17.19%), Space: 71.6 MB (5.01%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        HashSet<Long> seen = new HashSet<>();
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        seen.add(1l);
+        minHeap.add(1l);
+        
+        int[] multiply = {2, 3, 5};
+        
+        for(int i = 1; i < n; i++){
+            long num = minHeap.poll();
+            
+            for(int m : multiply){
+                if(!seen.contains(num * m)){
+                    seen.add(num * m);
+                    minHeap.add(num * m);
+                }
+            }
+        }
+        
+        return (int) (long) minHeap.peek();
+    }
+}
