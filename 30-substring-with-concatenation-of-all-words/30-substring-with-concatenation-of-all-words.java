37373153ceb1080077d069c4aeb4bb97d58fe559
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-06-28 16:26:18 +0300

    Time: 2609 ms (5.03%), Space: 158 MB (5.03%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        HashMap<Integer, String> wordFromIdx = new HashMap<>();
+        int len = words[0].length();
+        for (int idx = 0; idx + len <= s.length(); idx++) {
+            String maybe = s.substring(idx, idx + len);
+            if (map.containsKey(maybe)) {
+                wordFromIdx.put(idx, maybe);
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int idx = 0; idx < s.length(); idx++) {
+            HashMap<String, Integer> _map = new HashMap<>();
+            int itr = idx, usedWords = 0;
+
+            while (itr + len <= s.length()) {
+                String maybe = wordFromIdx.getOrDefault(itr, "");
+                if (_map.getOrDefault(maybe, 0) >= map.getOrDefault(maybe, 0)) {
+                    break;
+                }
+
+                _map.put(maybe, _map.getOrDefault(maybe, 0) + 1);
+                itr += len;
+                usedWords++;
+            }
+
+            if (usedWords == words.length) {
+                ans.add(idx);
+            }
+        }
+
+        return ans;
+    }
+}
