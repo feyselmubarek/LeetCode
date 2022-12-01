5b36b38950a4a6a8c9131bf353b9baca7d57ddd5
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-12-01 13:24:36 +0300

    Time: 50 ms (52.29%), Space: 52.1 MB (79.12%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,15 +1,35 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        if(s.length() < 10) return new ArrayList<>();
+        int hash = 0;
+        int MOD = (int) 1e9 + 7;
         HashSet<String> ans = new HashSet<>();
-        HashSet<String> set = new HashSet<>();
-        for(int i = 0; i <= s.length() - 10; i++){
-            String maybe = s.substring(i, i + 10);
-            if(set.contains(maybe)){
-                ans.add(maybe);
-            }
-            set.add(maybe);
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 9; i >= 0; i--){
+            hash += val(s.charAt(i)) * (int) Math.pow(4, 9 - i);
         }
+        
+        set.add(hash);
+        // System.out.println(hash);
+        for(int i = 10; i < s.length(); i++){
+            int left = i - 10;
+            hash -= val(s.charAt(left)) * (int) Math.pow(4, 9);
+            hash *= 4;
+            hash += val(s.charAt(i));
+            
+            if(set.contains(hash)){
+                ans.add(s.substring(i - 9, i + 1));
+            }
+            set.add(hash);
+            // System.out.println(hash);
+        }
+        
         return new ArrayList<>(ans);
+    }
+    
+    public int val(char c){
+        return c == 'A' ? 1 : c == 'C' ? 2 : c == 'G' ? 3 : 4;
     }
     
 }
