1938d6ff91004ada3395380a409f86cd04cbf48f
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-12-01 13:01:51 +0300

    Time: 54 ms (42.12%), Space: 64.3 MB (33.54%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,19 +1,6 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans = new ArrayList<>();
-        if(s.length() < 10){
-            return ans;
-        }
-        
-//         int MOD = (int) 1e5 + 7;
-//         int hash = 0;
-        
-//         for(int i = 9; i >= 0; i--){
-//             int val = (int) (((s.charAt(i) - '0') * Math.pow(26, i)) % MOD); 
-//             hash += val; 
-//         }
-        
-        // HashMap<Integer, HashSet<String>> map = new HashMap<>();
+        HashSet<String> ans = new HashSet<>();
         HashSet<String> set = new HashSet<>();
         for(int i = 0; i <= s.length() - 10; i++){
             String maybe = s.substring(i, i + 10);
@@ -22,7 +9,7 @@ class Solution {
             }
             set.add(maybe);
         }
-        return new ArrayList<>(new HashSet<>(ans));
+        return new ArrayList<>(ans);
     }
     
 }
