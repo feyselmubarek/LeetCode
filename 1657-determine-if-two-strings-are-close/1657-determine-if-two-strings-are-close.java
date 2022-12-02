d69f5d4f4e21fe331237be8ed564e4833cdaaa90
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2022-12-02 22:14:20 +0300

    Time: 11 ms (100.00%), Space: 43 MB (89.78%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,31 +1,29 @@
 class Solution {
+    private int N = 26;
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length() != word2.length()){
-            return false;
-        }
-        
-        int[] counts = new int[26];
-        for(int idx = 0; idx < word1.length(); idx++){
-            counts[word1.charAt(idx) - 'a']++;
-        }
-         
-        int[] counts2 = new int[26];
-        for(int idx = 0; idx < word2.length(); idx++){
-            counts2[word2.charAt(idx) - 'a']++;
-            if(counts[word2.charAt(idx) - 'a'] == 0){
+		// count the English letters
+        int[] arr1 = new int[N], arr2 = new int[N];
+        for (char ch : word1.toCharArray())
+            arr1[ch - 'a']++;
+        for (char ch : word2.toCharArray())
+            arr2[ch - 'a']++;
+		
+		// if one has a letter which another one doesn't have, dont exist
+        for (int i = 0; i < N; i++) {
+            if (arr1[i] == arr2[i]) {
+                continue;
+            }
+            if (arr1[i] == 0 || arr2[i] == 0) {
                 return false;
             }
         }
-        
-        Arrays.sort(counts);
-        Arrays.sort(counts2);
-        
-        for(int i = 0; i < 26; i++){
-            if(counts[i] != counts2[i]){
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for (int i = 0; i < N; i++) {
+            if (arr1[i] != arr2[i]) {
                 return false;
             }
         }
-
         return true;
     }
 }
