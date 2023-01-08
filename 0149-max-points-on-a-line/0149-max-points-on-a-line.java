7a06f29a73ba31479333d366a8dcd959a8af7d23
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2023-01-08 20:22:42 +0300

    Time: 15 ms (95.22%), Space: 42.2 MB (82.37%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,42 +1,31 @@
 class Solution {
     public int maxPoints(int[][] points) {
         int len = points.length, best = 0;
-        HashMap<String, Integer> lines = new HashMap<>();
+        HashMap<Double, Integer> lines = new HashMap<>();
         
         for(int idx = 0; idx < len; idx++){
             int[] pointA = points[idx];
             lines = new HashMap<>();
-            int max = 1, verticalLinePts = 1, overlapping = 0;
+            int max = 1, verticalLinePts = 1;
             
             for(int itr = idx + 1; itr < len; itr++){
                 int[] pointB = points[itr];  
                 int changeY = pointB[1] - pointA[1];
                 int changeX = pointB[0] - pointA[0];
-                int gcd = calculateGcd(changeX, changeY);
                 
-                if(changeY == 0 && changeX == 0){
-                    overlapping++;
-                }else if(changeX == 0){
+                if(changeX == 0){
                     verticalLinePts++;
                 }else{
-                    String slope = (changeY / gcd) + "/" + (changeX / gcd);
+                    double slope = changeY == 0 ? 0 : (double) changeY / (double) changeX;
                     int pts = lines.getOrDefault(slope, 1);
                     lines.put(slope, pts + 1);
                     max = Math.max(max, pts + 1);
                 }
             }
             
-            best = Math.max(best, Math.max(verticalLinePts, max) + overlapping);
+            best = Math.max(best, Math.max(verticalLinePts, max));
         }
         
         return best;
-    }
-    
-    public int calculateGcd(int a, int b){
-        if(b == 0){
-            return a;
-        }
-        
-        return calculateGcd(b, a % b);
     }
 }
