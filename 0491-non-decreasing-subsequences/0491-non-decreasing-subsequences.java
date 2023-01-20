aa42b348edaa3dc55979effc09d7213d474be934
Feysel Mubarek <39192666+feyselmubarek@users.noreply.github.com>
2023-01-20 22:00:05 +0300

    Time: 13 ms (63.09%), Space: 49.4 MB (65.27%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,26 +1,31 @@
 class Solution {
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        int n = nums.length;
-        Set<List<Integer>> result = new HashSet<List<Integer>>();
-        for (int bitmask = 1; bitmask < (1 << n); bitmask++) {
-            List<Integer> sequence = new ArrayList<Integer>();
-            // check the i-th bit of the bitmask
-            for (int i = 0; i < n; i++) {
-                if (((bitmask >> i) & 1) == 1) {
-                    sequence.add(nums[i]);
-                }
-            }
+    private void backtrack(int[] nums, int index, List<Integer> sequence,
+            Set<List<Integer>> result) {
+        // if we have checked all elements
+        if (index == nums.length) {
             if (sequence.size() >= 2) {
-                // check whether the sequence is increasing
-                boolean isIncreasing = true;
-                for (int i = 0; i < sequence.size() - 1; i++) {
-                    isIncreasing &= sequence.get(i) <= sequence.get(i + 1);
-                }
-                if (isIncreasing) {
-                    result.add(sequence);
-                }
+                result.add(new ArrayList<>(sequence));
             }
+            return;
         }
+        // if the sequence remains increasing after appending nums[index]
+        if (sequence.isEmpty() ||
+                sequence.get(sequence.size() - 1) <= nums[index]) {
+            // append nums[index] to the sequence
+            sequence.add(nums[index]);
+            // call recursively
+            backtrack(nums, index + 1, sequence, result);
+            // delete nums[index] from the end of the sequence
+            sequence.remove(sequence.size() - 1);
+        }
+        // call recursively not appending an element
+        backtrack(nums, index + 1, sequence, result);
+    }
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> result = new HashSet<List<Integer>>();
+        List<Integer> sequence = new ArrayList<Integer>();
+        backtrack(nums, 0, sequence, result);
         return new ArrayList(result);
     }
 }
